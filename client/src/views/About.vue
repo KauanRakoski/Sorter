88ff7a2c432cec8d268b82c977490c250f866032
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-09-25 22:34:24 -0300

    add router, views

---
+++ client/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1 class="px-3 pt-4">Welcome to sorter!</h1>
+    <p class="h5 px-3 pt-3">Sorter is an open-source project built on top of Vue.js (firebase in the front-end) and express. If you want to contribute, visit <a target="_blank" href="https://github.com/kauanrakoski/sorter">Github</a>, everything is there.</p>
+  </div>
+</template>
